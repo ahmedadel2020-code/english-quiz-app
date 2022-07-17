961b9fda7d72ac38e43ef299f68b240949ff9ed1
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2022-07-17 21:10:00 +0200

    finish backend

--- Server/controllers/rankController.js
+++ Server/controllers/rankController.js
@@ -1,15 +1,26 @@
+const { scoresList } = require("../utils/TestData.json");
+
 // @desc    Set Rank
 // @route   POST /api/rank
 // @access  Private
 const setRank = (req, res) => {
   if (!req.body.score) {
     res.status(400);
-    throw new Error("Please add a rank field");
+    throw new Error("Please add a score field");
   }
 
   const score = req.body.score;
+  let scoreBelow = 0;
 
-  res.status(200).json({ rank: score });
+  for (let i = 0; i < scoresList.length; i++) {
+    if (scoresList[i] < score) {
+      scoreBelow++;
+    }
+  }
+
+  const rank = ((scoreBelow / scoresList.length) * 100).toFixed(2);
+
+  res.status(200).json({ rank });
 };
 
 module.exports = {
